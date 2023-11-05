5766a3f99820c470ee233f5aaeab08ecb387987e
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-05 20:51:53 +0530

    if message send from one browser can be shown in other browser

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const socket = io("http://localhost:3011");
+const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
 socket.on("chat-message", (data) => {
-  console.log(data);
+  console.log(data, "data");
+  //
+  appendMessage(data);
 });
 //
 messageForm.addEventListener("submit", (e) => {
@@ -15,3 +18,10 @@ messageForm.addEventListener("submit", (e) => {
   //empty out message after every sent
   messageInput.value = "";
 });
+
+//
+function appendMessage(message) {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(message);
+}
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ io.on("connection", (socket) => {
   // socket.emit("chat-message", "Hello-world1");
   //
   socket.on("send-chat-message", (message) => {
-    console.log(message);
+    console.log(message, "message");
     socket.broadcast.emit("chat-message", message);
     //this sends everone connected to the server, except who sends it
     // socket.broadcast.emit;
