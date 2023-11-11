c7c896954b9250c6b9cb5273323faf1ce920e012
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-11 19:34:01 +0530

    try2

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const socket = io("http://localhost:3011");
+
+//
 const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
 //
-const usedName = prompt("what is your name?");
+const takeName = prompt("what is your name?");
 appendMessage("You joined");
-socket.emit("new-user", usedName);
+//sends - to server that new- user joined-to server
+socket.emit("new-user", takeName);
 
 socket.on("chat-message", (data) => {
   console.log(data, "data");
-  //
   appendMessage(`${data.name}: ${data.message}`);
+  // appendMessage(data);
 });
 
 //
@@ -24,7 +27,8 @@ socket.on("user-disconnected", (name) => {
   //   console.log(name, "name");
   appendMessage(`${name} disconnected`);
 });
-//
+// //
+// client er message -- jeta form e likhche
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = messageInput.value;
@@ -32,15 +36,17 @@ messageForm.addEventListener("submit", (e) => {
   //
   appendMessage(`You: ${message}`);
 
-  //"send-chat-message"--event name
+  //"send-chat-message"--event name---user sending to server
   socket.emit("send-chat-message", message);
   //empty out message after every sent
   messageInput.value = "";
 });
 
-//
+// client jei message pathacche-- seta frontend e dekhano
 function appendMessage(message) {
   const messageElement = document.createElement("div"); // Create a new div for each message
-  messageElement.innerHTML = message.replace(/\n/g, "<br>"); // Use innerHTML and replace line breaks with <br>
-  messageContainer.appendChild(messageElement); // Append the message div to the container
+  // messageElement.innerHTML = message.replace(/\n/g, "<br>"); // Use innerHTML and replace line breaks with <br>
+  //
+  messageElement.innerHTML = message;
+  messageContainer.append(messageElement); // Append the message div to the container
 }
--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 const io = require("socket.io")(3011, {
   cors: {
-    origin: "*",
+    // origin: "*",
+    origin: ["http://127.0.0.1:5500"],
   },
 });
 
 const users = {};
+//aaa
 
 io.on("connection", (socket) => {
-  // socket.emit("chat-message", "Hello-world1");
+  socket.emit("chat-message", "Hello-world-folks");
+  console.log("new User");
+
+  //server -recives - from client that new- user joined-to server
   socket.on("new-user", (name) => {
     users[socket.id] = name;
+
+    console.log(users, "users---");
     //
     socket.broadcast.emit("user-connected", name);
   });
-  //
+  // //
+  // send by server recved by the client
   socket.on("send-chat-message", (message) => {
-    console.log(message, "message");
+    // console.log(message, "--message");
+    //this send from one client --- and his messege would be send to all other clients, except him
+    // socket.broadcast.emit("chat-message", message);
+    //
     socket.broadcast.emit("chat-message", {
       message: message,
       name: users[socket.id],
     });
+    //
+    //
   });
   //disconnect
   socket.on("disconnect", () => {
