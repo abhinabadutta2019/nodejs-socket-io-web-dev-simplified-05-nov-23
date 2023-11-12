9b96aaa1bb94466dfc1a676a417629451a14d743
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-12 20:48:23 +0530

    sending message from one to another

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,18 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
   //
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+  //
   socket.on("send_message", (data) => {
     //
-    console.log(data, "-send_message--data");
+    socket.to(data.room).emit("recived_message", data);
+    // console.log(data, "-send_message--data");
+    // console.log(data.room);
 
     //
-    socket.emit("recived_message", data);
+    // socket.emit("recived_message", data);
   });
 });
 
