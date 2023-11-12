a6f261cd4cd5b4fd8600ffa0319f9f9cb3f67c0b
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-12 22:34:03 +0530

    message + user showing

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
 
-//
 const app = express();
 app.use(cors());
 
@@ -14,38 +13,25 @@ const io = new Server(server, {
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
   },
 });
-//
+
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
-  //
+
   socket.on("join_room", (data) => {
-    //
     const { room, userName } = data;
     socket.join(room);
-    //
-    // socket.join(data);
     console.log(`${userName} joined room ${room}`);
   });
-  //
+
   socket.on("send_message", (data) => {
-    //
     const { message, room, userName } = data;
-    // socket.to(data.room).emit("recived_message", data);
-    socket.to(room).emit("recived_message", { message, userName, room });
+    socket.to(room).emit("recived_message", { message, userName });
     console.log(`${userName} sent a message to room ${room}: ${message}`);
-
-    // console.log(data, "-send_message--data");
-    // console.log(data.room);
-
-    //
-    // socket.emit("recived_message", data);
   });
 });
-
-//
 
 const PORT = 3014;
 
 server.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
